053bd09421085521aa7be616aa8cfd93fad52556
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-31 06:17:51 +0300

    Add HomeSwiper component and update Home page layout

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,30 @@
 import { Flex, Text } from "@mantine/core";
 import { getIPData } from "@/app/actions";
+import HomeSwiper from "@/components/home/HomeSwiper";
 export default async function Home() {
-  const data = await getIPData();
-  return <Flex h={"100vh"} justify="center" direction={"column"} >
-  </Flex>;
+  return (
+    <Flex h="80vh" gap={10} >
+      <Flex bg={"red"} w="20%">
+        Menu
+      </Flex>
+      <Flex w="80%" gap={10} direction="column">
+        <Flex bg="grape" w="100%" h="200px">
+          Header
+        </Flex>
+        <Flex h="100%" gap={10}>
+          <Flex w="70%" gap={10} direction="column">
+            <Flex bg="yellow" h="70%" >
+              <HomeSwiper />
+            </Flex>
+            <Flex bg="violet" h="30%">
+              Offers
+            </Flex>
+          </Flex>
+          <Flex bg="pink" w="30%">
+            User
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
 }
