8683d295e5eece10bad401d0d7356d245f8e9d80
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-31 07:22:20 +0300

    Add FlashSale component to Home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text } from "@mantine/core";
 import { getIPData } from "@/app/actions";
 import HomeSwiper from "@/components/home/HomeSwiper";
+import FlashSale from "@/components/home/FlashSale";
 export default async function Home() {
   return (
     <Flex h="80vh" gap={10} >
@@ -8,16 +9,16 @@ export default async function Home() {
         Menu
       </Flex>
       <Flex w="80%" gap={10} direction="column">
-        <Flex bg="grape" w="100%" h="200px">
+        <Flex bg="grape" w="100%" h="100px">
           Header
         </Flex>
         <Flex h="100%" gap={10}>
           <Flex w="70%" gap={10} direction="column">
-            <Flex bg="yellow" h="70%" >
+            <Flex h="60%" >
               <HomeSwiper />
             </Flex>
-            <Flex bg="violet" h="30%">
-              Offers
+            <Flex h="40%" >
+              <FlashSale />
             </Flex>
           </Flex>
           <Flex bg="pink" w="30%">
